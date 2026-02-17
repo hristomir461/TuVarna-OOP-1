d5c3b28616b22331a8821189f6b5317ed19dc3fe
hristomir461 <hristomir.hari@gmail.com>
2026-02-17 19:06:59 +0200

    project created

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+/**
+ * Entry point for the application.
+ *
+ * <p>This class contains the {@link #main(String[])} method, which is the first
+ * method executed when the program starts.</p>
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("TuVarna-OOP-1");
+    }
+}
